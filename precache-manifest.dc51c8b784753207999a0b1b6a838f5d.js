702ad4d1ade0ba1a5a4191f2cca8383011654b79
tergel <oyuerdenetergel460@gmail.com>
2021-01-05 01:01:45 +0800

    Updates

---
+++ precache-manifest.dc51c8b784753207999a0b1b6a838f5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7fa44d0469570ed4350282e339efae",
+    "url": "/challangeBlogFront/index.html"
+  },
+  {
+    "revision": "6c51eb96b801355a2800",
+    "url": "/challangeBlogFront/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "c1abf19374187b00a196",
+    "url": "/challangeBlogFront/static/js/2.8f9ebcd6.chunk.js"
+  },
+  {
+    "revision": "9c4fbc8ff90975212320619d479d493e",
+    "url": "/challangeBlogFront/static/js/2.8f9ebcd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c51eb96b801355a2800",
+    "url": "/challangeBlogFront/static/js/main.35c57670.chunk.js"
+  },
+  {
+    "revision": "99b9f07edd430ad49e5f",
+    "url": "/challangeBlogFront/static/js/runtime-main.247bdcc8.js"
+  },
+  {
+    "revision": "25efde8f9eefc895a26597e046570549",
+    "url": "/challangeBlogFront/static/media/logo.25efde8f.png"
+  },
+  {
+    "revision": "109eb569d000d14b540c6119a3415920",
+    "url": "/challangeBlogFront/static/media/yuno.109eb569.jpg"
+  }
+]);
